1099f18beddb1b28c4c99d3d91e77b66baa861ab
CozyProgrammer <mhasjaved01@gmail.com>
2025-09-22 00:00:34 +0500

    Addition of components

--- Bank.java
+++ Bank.java
@@ -1,37 +1,56 @@
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Bank{
    static Scanner sc =new Scanner(System.in);
+   static Random r=new Random();
     public static void main(String[] args) {
-        
-       System.out.println("Enter Your Name :");
-       String name=sc.next();
-
-       System.out.println("Enter your Address :");
-       String address=sc.next();
-
-       Customer customer=new Customer(name, address);
-
-       System.out.println("Enter Your Account Number :");
-       int accNo=sc.nextInt();
-
-       System.out.println("Enter Your Initial Balance :");
-       double balance=sc.nextDouble();
-
-       Account account=new Account(accNo, balance, customer);
-
+       
        HashMap <Integer,Account> info=new HashMap<>();
 
-       System.out.println("Choose One Of the Following Options :");
+       System.out.println("Hello and Welcome");
+
+       System.out.println("Please Choose One Of the Following Options :");
        System.out.println("1.Create Account ");
        System.out.println("2.Deposit");
        System.out.println("3.Withdraw");
        System.out.println("4.Check Balance");
        System.out.println("5.Exit");
 
-       
-        
-    }
+       boolean True=true;
 
+       while(True){
+         int n=sc.nextInt();
+
+         switch(n){
+            case 1:{ System.out.println("So you wanna create an a new account");
+             System.out.println("Enter Your Name :");
+             String name=sc.nextLine();
+
+             System.out.println("Enter your Address :");
+             String address=sc.nextLine();
+
+             Customer customer=new Customer(name, address);
+
+             int accNo=r.nextInt(100000,250000);
+
+             System.out.println("Enter Amount you want to add in Account (Minimum Rs.1000) :");
+             double balance=sc.nextDouble();
+
+             boolean T=true;
+
+       while (T){
+          if(balance>=1000){
+             Account account=new Account(accNo, balance, customer);
+             break;
+       }
+        else {
+             System.out.println("Please Enter Minimum Amount At least");
+          }
+        }
+      }
+    }
+   }     
+ }
 }
